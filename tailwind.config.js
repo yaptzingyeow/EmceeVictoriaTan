89a81a16d1706412481924469b6e158e0878e115
Salesworks-TzingYeow <yaptzingyeow@gmail.com>
2025-09-14 23:34:03 +0800

    commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./index-zh.html",
+    "./**/*.html",
+    "./assets/**/*.js",
+    "./scripts/**/*.js"
+  ],
+  theme: { extend: {} },
+  plugins: [],
+};
